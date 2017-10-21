4c17e6108191deb52290e397d36672455de70eaa
Aljaz Ceru <aljaz@ceru.si>
2017-10-21 19:12:19 +0200

    adding support for keyword scan

--- s3scan.py
+++ s3scan.py
@@ -17,13 +17,43 @@ def parseXml(result, link):
         pass
 
 
+def wordlistScan(bucket):
+    print "scanning bucket: " + bucket
+    link = "http://" + bucket + ".s3.amazonaws.com"
+    try:
+        r = requests.head(link)
+        if r.status_code != 404:
+            r = requests.get(link)
+            parseXml(r.text, link)
+    except requests.exceptions.RequestException as e:
+        print e
+        pass
+
+def keywordScan(bucket):
+    print "scanning bucket: " + bucket
+    link = "http://" + bucket + ".s3.amazonaws.com"
+    try:
+        r = requests.head(link)
+        if r.status_code != 404:
+            r = requests.get(link)
+            parseXml(r.text, link)
+    except requests.exceptions.RequestException as e:
+        print e
+        pass
+
 def main():
     wordlist = ""
-
+    keyword = ""
     parser = ArgumentParser()
     parser.add_argument("-w", "--wordlist", dest="wordlist",
                         help="Wordlist to use for bucket names (default: wordlist.txt)", default="wordlist.txt",
                         metavar="wordlist")
+
+    parser.add_argument("-k", "--keyword", dest="keyword",
+                        help="Keyword to use with the wordlist in the form of <keyword>-<wordlist>", default="",
+                        metavar="keyword")
+
+
 
     if len(sys.argv) == 1:
         parser.print_help()
@@ -35,15 +65,11 @@ def main():
         bucketNames = [line.strip() for line in f]
 
     for bucket in bucketNames:
-        link = "http://" + bucket + ".s3.amazonaws.com"
-        try:
-            r = requests.head(link)
-            if r.status_code != 404:
-                r = requests.get(link)
-                parseXml(r.text, link)
-        except requests.exceptions.RequestException as e:
-            print e
-            pass
+        if args.keyword != "":
+            target = args.keyword + "-" + bucket
+            keywordScan(target)
+        else:
+            wordlistScan(bucket)
 
 
 if __name__ == "__main__":
